07f297c1ca26745f30e3e21372fd5e93790668d4
ERFAN <erfansafarzad7@gmail.com>
2025-01-06 16:53:44 +0330

    init db

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from .database.database import engine
+from .models.models import Base
+
+
+Base.metadata.create_all(bind=engine)
+
---
+++ app/models/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    polls = relationship('Pool', back_populates="owner")
+    votes = relationship('Vote', back_populates="voter")
+
+
+class Poll(Base):
+    __tablename__ = "polls"
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship('User', back_populates='polls')
+    options = relationship('Option', back_populates='poll')
+    votes = relationship('Vote', back_populates='poll')
+
+
+class Option(Base):
+    __tablename__ = "options"
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, index=True)
+    poll_id = Column(Integer, ForeignKey('polls.id'))
+
+    poll = relationship('Poll', back_populates='options')
+    votes = relationship('Vote', back_populates='option')
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    id = Column(Integer, primary_key=True, index=True)
+    poll_id = Column(Integer, ForeignKey("polls.id"))
+    option_id = Column(Integer, ForeignKey("options.id"))
+    voter_id = Column(Integer, ForeignKey("users.id"))
+
+    poll = relationship('Poll', back_populates='votes')
+    option = relationship('Option', back_populates='votes')
+    voter = relationship('User', back_populates='votes')
+
+
+
+
+
